0c0b5ffceee57f4365ce0012702d730a69fb7480
dingwy <471240186@qq.com>
2020-11-19 18:45:49 +0800

    三个数的最大乘积

--- leetcode/problems.go
+++ leetcode/problems.go
@@ -2,6 +2,7 @@ package leetcode
 
 import (
 	"math"
+	"sort"
 )
 
 /**
@@ -253,4 +254,27 @@ func findRepeatNumber(nums []int) int {
 		numMap[num] = 1
 	}
 	return -1
+}
+
+/*
+三个数的最大乘积
+*/
+func maximumProduct(nums []int) int {
+	sort.Ints(nums)
+	length := len(nums)
+	maxArr := nums[length-3:]
+	maxArr2 := nums[0:3]
+	maxRes := 0
+	res1 := maxArr[0] * maxArr[1] * maxArr[2]
+	res2 := maxArr2[0] * maxArr2[1] * maxArr2[2]
+	res3 := maxArr2[0] * maxArr2[1] * maxArr[2]
+
+	maxRes = res2
+	if res1 > res2 {
+		maxRes = res1
+	}
+	if res3 > maxRes {
+		maxRes = res3
+	}
+	return maxRes
 }
