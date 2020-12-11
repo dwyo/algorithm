c51f99d110ac4035b251ef3d171a9287fa620772
dingwy <471240186@qq.com>
2020-12-11 10:24:48 +0800

    Dota2 参议院

---
+++ leetcode/649dota2参议院_test.go
@@ -0,0 +1,14 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test649dota2参议院_predictPartyVictory(t *testing.T) {
+
+	s := "RD"
+
+	res := predictPartyVictory(s)
+	fmt.Println("获胜方：", res)
+}
