cef52eebd851720d3de261f4349a2c1427bfca5b
dingwy <471240186@qq.com>
2020-12-25 10:29:12 +0800

    分发饼干

---
+++ leetcode/455分发饼干_test.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test455分发饼干_findContentChildren(t *testing.T) {
+
+	g := []int{1, 2}
+	s := []int{1, 2, 3}
+	fmt.Println(findContentChildren(g, s))
+}
