24fc9c6a78e381515816b4f01b190d5f08887de0
dingwy <471240186@qq.com>
2021-01-21 14:17:41 +0800

    相似的树

---
+++ leetcode/editor/cn/[101]对称二叉树.go
@@ -0,0 +1,62 @@
+package leetcode
+
+//给定一个二叉树，检查它是否是镜像对称的。
+//
+// 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+// 
+//
+// 
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索 
+// 👍 1206 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+
+	if root == nil {
+		return true
+	}
+	return isEq(root.Left, root.Right) && isEq(root.Right, root.Left)
+}
+
+func isEq(l, r *TreeNode) bool {
+	if l == nil && r == nil {
+		return true
+	}
+	if l == nil || r == nil || l.Val != r.Val {
+		return false
+	}
+	return isEq(l.Left, r.Right) && isEq(r.Right, l.Left)
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
